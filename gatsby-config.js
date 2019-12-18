c2991f47189258f4e7558e70a2d47d66195baccf
JamesKemp296 <jameskemp2796@yahoo.com>
2019-12-17 21:39:25 -0500

    metadata changed from default

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    title: `Webdev Blog`,
+    author: `James, Andres M, Andres U, Will`,
+    description: `A blog about all things web development.`,
+    siteUrl: `Coming soon`,
   },
   plugins: [
     {
